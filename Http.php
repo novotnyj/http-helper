ab8d6f6f3f4e81b25b0ebe3b76d62036202507e7
Jan Novotny <naj.yntovon@gmail.com>
2014-01-08 14:03:54 +0100

    Moved license to php file

--- Http.php
+++ Http.php
@@ -1,5 +1,37 @@
 <?php
 
+/*
+New BSD License
+
+Copyright (c) 2014 Jan Novotny (naj.yntovon@gmail.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	* Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution.
+
+	* Neither the name of author nor the names of its contributors
+	may be used to endorse or promote products derived from this software
+	without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall the copyright owner or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused and on
+any theory of liability, whether in contract, strict liability, or tort
+(including negligence or otherwise) arising in any way out of the use of this
+software, even if advised of the possibility of such damage.
+*/
+
 namespace Http;
 
 class Request {
@@ -18,7 +50,7 @@ class Request {
 
 	private $headers = array();
 
-	private $method = self::GET;
+	private $method;
 
 	private $hasUrl = FALSE;
 
@@ -36,12 +68,13 @@ class Request {
 
 	private $rawUrl = '';
 
-	public function __construct($url = NULL){
+	public function __construct($url = NULL, $method = NULL){
 		$this->response = new Response();
 		$this->handle = curl_init();
 		@curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, 1);
 		@curl_setopt($this->handle, CURLOPT_HEADER, 1);
 		if ($url) $this->setUrl($url);
+		if ($method) $this->setMethod($method);
 	}
 
 	public function setMethod($method){
